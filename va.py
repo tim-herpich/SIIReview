334423f80891c5055f96350936c352c741b3698b
Tim Herpich <Tim.Herpich@d-fine.de>
2025-01-14 15:00:22 +0100

    SII Review Tool: Mocking class for new VA.

--- va.py
+++ va.py
@@ -5,17 +5,83 @@ class VASpreadCalculator:
 
     def __init__(self):
         pass
-    
-    def compute_va_spread(self):
+
+    def compute_total_va(self):
         """
         Computes the VA spread based on the provided zero rates and other parameters.
         Args:
-            zero_rates (numpy.ndarray): Array of bootstrapped zero rates.
-            dlt_array (numpy.ndarray): Array of day count fractions (DLT).
-            weights_array (numpy.ndarray): Array of weights (LLFR Weights).
-
         Returns:
             VA spread in bp
         """
-        return 10.0
-    
+        return self.compute_macro_va_() + self.compute_currency_va_()
+
+    def compute_macro_va_(self):
+        """
+        Computes the macroeconomic VA spread based on the provided zero rates and other parameters.
+        Args:
+        Returns:
+            Macro VA spread in bp
+        """
+
+        # va_macro = 0.85*self.compute_cssr_cu()*max(self.compute_rcs_co-1.3*self.compute_rcs_cu, 0)*w_co
+
+        return 5.0
+
+    def compute_currency_va_(self):
+        """
+        Computes the currency VA spread based on the currency-specific credit spread sensitive ratio and the risk-corrected spread
+        Args:
+        Returns:
+            Currency-VA spread in bp
+        """
+        # va_cu = 0.85*self.compute_cssr_cu()*self.compute_rcs_cu()
+
+        return 5.0
+
+    def compute_cssr_cu(self):
+        """
+        Computes the currency-specific credit spread sensitive ratio (CSSR)
+        Args:
+        Returns:
+            Currency-specific CSSR
+        """
+
+        # compute max(min( [(FI_A-FI_A*)/VA*]/(BE_L-BE_L*)/VA*,1),0)
+
+        return 1.0
+
+    def compute_rcs_cu(self):
+        """
+        Computes the currency-specific risk-corrected spread (RCS)
+        Args:
+        Returns:
+            Currency-specific RCS
+        """
+
+        # compute S_cu - RC_cu
+
+        return 1.0
+
+    def compute_rcs_co(self):
+        """
+        Computes the country-specific risk-corrected spread (RCS)
+        Args:
+        Returns:
+            Country-specific RCS
+        """
+
+        # compute S_co - RC_co
+
+        return 1.0
+
+    def compute_w_co(self):
+        """
+        Computes the country-specific adjustment factor w_co
+        Args:
+        Returns:
+            Country-specific w_co
+        """
+
+        # w_co = max( min([self.compute_rcs_co * FI / Assets - 0.6%] / 0.3%, 1),0)
+
+        return 1.0
